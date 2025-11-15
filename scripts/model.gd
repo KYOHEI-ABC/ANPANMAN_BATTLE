eaf64eb296ec86abb1d6a575b02d7ffe0f54b608
KyoheiAbc <autumn8november@gmail.com>
2025-11-15 14:54:50 +0900

    Refactor character initialization; update process function for movement handling

--- scripts/model.gd
+++ scripts/model.gd
@@ -6,6 +6,7 @@ const PACKED_SCENES: Array[PackedScene] = [
 	preload("res://assets/b.gltf"),
 ]
 
+var character: Character
 
 var root: Node3D = null
 var right_arm: Node3D
@@ -13,11 +14,17 @@ var left_arm: Node3D
 var right_leg: Node3D
 var left_leg: Node3D
 
-func _init(id: int):
+var walk_count: int = 0
+
+func _init(character: Character):
+	self.character = character
+
 	var scene = PackedScene.new()
-	var gltf = PACKED_SCENES[id].instantiate()
+	var gltf = PACKED_SCENES[self.character.id].instantiate()
 	scene.pack(gltf)
 	root = scene.instantiate()
+	root.position.y = - character.size.y / 200
+
 	add_child(root)
 	gltf.queue_free()
 
@@ -27,3 +34,21 @@ func _init(id: int):
 	left_arm = root.get_node("left_arm")
 	right_leg = root.get_node("right_leg")
 	left_leg = root.get_node("left_leg")
+
+func process():
+	var new_position = Vector3(character.position.x / 100, -character.position.y / 100, 0)
+	if (new_position - self.position).length_squared() > 0.001:
+		var phase = walk_count / 5.0
+		right_arm.rotation_degrees.x = sin(phase) * 45
+		left_arm.rotation_degrees.x = - sin(phase) * 45
+		right_leg.rotation_degrees.x = - sin(phase) * 45
+		left_leg.rotation_degrees.x = sin(phase) * 45
+		walk_count += 1
+	else:
+		walk_count = 0
+		right_arm.rotation_degrees.x = 0
+		left_arm.rotation_degrees.x = 0
+		right_leg.rotation_degrees.x = 0
+		left_leg.rotation_degrees.x = 0
+
+	self.position = new_position

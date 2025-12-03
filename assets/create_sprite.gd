c47ebf6dd4bf9703e4281ef856f17c026cfaa9c9
KyoheiAbc <autumn8november@gmail.com>
2025-12-03 21:18:38 +0900

    Add new assets and scripts for sprite creation and background handling

---
+++ assets/create_sprite.gd
@@ -0,0 +1,40 @@
+extends SceneTree
+
+const SIZE = 128
+
+const ASSETS = [
+	"a.gltf",
+	"b.gltf",
+]
+
+func _init():
+	RenderingServer.set_default_clear_color(Color.from_hsv(0, 0, 0, 0))
+
+	ProjectSettings.set_setting("display/window/size/viewport_width", SIZE)
+	ProjectSettings.set_setting("display/window/size/viewport_height", SIZE)
+
+	var root = get_root()
+	root.transparent_bg = true
+
+	var camera = Camera3D.new()
+	root.add_child(camera)
+	camera.position = Vector3(0, 0, 8)
+	camera.projection = Camera3D.PROJECTION_ORTHOGONAL
+	camera.size = 0.8
+	var light = DirectionalLight3D.new()
+	camera.add_child(light)
+	light.shadow_enabled = false
+
+	for asset in ASSETS:
+		var gltf = load("res://assets/" + asset).instantiate()
+		root.add_child(gltf)
+		gltf.position.y = -1.2
+		gltf.rotation_degrees.y = -150
+	
+		await create_timer(1).timeout
+
+		root.get_viewport().get_texture().get_image().save_png(get_script().resource_path.get_base_dir() + "/" + asset.get_basename() + ".png")
+
+		gltf.queue_free()
+
+	quit()

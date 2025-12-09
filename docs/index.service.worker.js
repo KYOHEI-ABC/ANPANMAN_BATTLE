ba28cae5cb5f53632262b6e5429aad60f3bc32ec
KyoheiAbc <autumn8november@gmail.com>
2025-12-09 15:20:43 +0900

    Update Godot configuration and service worker cache version for improved resource management

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1765200359|11813165';
+const CACHE_VERSION = '1765261230|12204143';
 /** @type {string} */
 const CACHE_PREFIX = 'Godot Game-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
